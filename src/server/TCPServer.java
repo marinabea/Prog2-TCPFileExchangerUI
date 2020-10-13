bd8e1679ed110affa1840d4fae68b0cff793b3df
CarolinaTrack <carolinatrack@googlemail.com>
2020-10-13 15:05:07 +0200

    TCPserver angelegt / läuft jetzt Client: läuft noch nicht

--- src/server/TCPServer.java
+++ src/server/TCPServer.java
@@ -9,30 +9,34 @@ public class TCPServer {
     public static void main(String[] args) {
 
         int port = 3100;
+
         try {
             ServerSocket server = new ServerSocket(port);
             System.out.println("Server gestartet");
 
+            //client vom server akzeptieren lassen
             Socket client = server.accept();
 
             //Streams
-            //an den Client schicken:
-            OutputStream os = client.getOutputStream();
-            PrintWriter writer = new PrintWriter(os);
-
-            //vom Client bekommen
+            //ZUERST vom Client bekommen
             InputStream is = client.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-
             //Ausgabe der vom Client empfangenen Nachrichten:
             String empfangen = null;
             while ((empfangen = reader.readLine()) != null) {
                 System.out.println("Empfangen von Client: " + empfangen);
             }
 
+            //an den Client schicken:
+            OutputStream os = client.getOutputStream();
+            PrintWriter writer = new PrintWriter(os);
+            os.write("hallo Client".getBytes());
 
+
+            //Pause
             Thread.sleep(5000);
 
+            //writer und reader schließen
             writer.close();
             reader.close();
 
